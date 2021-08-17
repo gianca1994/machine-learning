978e22d9c8c86378d52bd25fcb7ef0137fa6e7b4
gianca1994 <gianca9405@gmail.com>
2021-08-16 23:22:22 -0300

    refactor code

--- tp1/exercise3.py
+++ tp1/exercise3.py
@@ -1,54 +1,10 @@
 import os
 from random import randint, choice
 from time import sleep
+from model_floor import Floor
 
 tiles = randint(3, 7)
 random_movement = randint(1, 2)
-
-
-class Floor:
-    def __init__(self, size, stage_list, stains_floor, position, move):
-        self.size = size
-        self.stage = stage_list
-        self.stains_floor = stains_floor
-        self.position = position
-        self.move = move
-
-    def get_size(self):
-        return self.size
-
-    def set_size(self, size):
-        self.size = size
-
-    def get_stage_list(self):
-        return self.stage
-
-    def set_stage_list(self, stage_list):
-        self.stage = stage_list
-
-    def get_stains_floor(self):
-        return self.stains_floor
-
-    def set_stains_floor(self, stains_floor):
-        self.stains_floor = stains_floor
-
-    def get_position(self):
-        return self.position
-
-    def set_position(self, position):
-        self.position = position
-
-    def move_left(self):
-        self.position -= 1
-
-    def move_right(self):
-        self.position += 1
-
-    def get_move(self):
-        return self.move
-
-    def set_move(self, move):
-        self.move = move
 
 
 class Funcion:
